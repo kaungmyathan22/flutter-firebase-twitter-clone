11c1669aeb7b5cb1bbe62fbe542966196e1ddfd1
Eric Han <eric_han@emf.com.sg>
2023-12-10 17:20:32 +0630

    chore(): basic firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -1,123 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:test/firebase_options.dart';
+import 'package:test/pages/login.dart';
 import 'package:test/proviers.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Setting',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const HomePage(),
+    return const MaterialApp(
+      home: LoginPage(),
       debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-class HomePage extends ConsumerWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    int counter = ref.watch(counterProvider);
-    CounterNotifier counterController = ref.watch(counterProvider.notifier);
-    return Scaffold(
-      appBar: AppBar(title: const Text("Riverpod simplified")),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextButton(
-                onPressed: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => const SecondExample()));
-                },
-                child: const Text("Second Widget")),
-            Text("Counter: $counter"),
-            Text(
-              ref.read(normalProvider),
-            ),
-            ref.watch(messageProvider).when(
-              data: (data) {
-                return Text(data);
-              },
-              error: (err, stackTrace) {
-                return const Text(
-                  "error",
-                  style: TextStyle(color: Colors.red),
-                );
-              },
-              loading: () {
-                return const CircularProgressIndicator();
-              },
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          counterController.add();
-        },
-        child: const Icon(
-          Icons.add,
-        ),
-      ),
-    );
-  }
-}
-
-class SecondExample extends ConsumerWidget {
-  const SecondExample({super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    int counter = ref.watch(counterProvider);
-    CounterNotifier counterController = ref.watch(counterProvider.notifier);
-    return Scaffold(
-      appBar: AppBar(title: const Text("Riverpod simplified")),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("same counter on second screen: $counter"),
-            Text(
-              ref.read(normalProvider),
-            ),
-            ref.watch(messageProvider).when(
-              data: (data) {
-                return Text(data);
-              },
-              error: (err, stackTrace) {
-                return const Text(
-                  "error",
-                  style: TextStyle(color: Colors.red),
-                );
-              },
-              loading: () {
-                return const CircularProgressIndicator();
-              },
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          counterController.add();
-        },
-        child: const Icon(
-          Icons.add,
-        ),
-      ),
     );
   }
 }
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:test/signup.dart';
+import 'package:test/pages/signup.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
